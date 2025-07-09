14dd0f99161ff19267d55fd47d2bc3d0c465d49b
Prince Rurangwa <p.rurangwa@alustudent.com>
2025-07-09 10:13:35 +0200

    Time: 70 ms (5.05%) | Memory: 56.5 MB (57.95%) - LeetSync

--- 2741-function-composition/function-composition.ts
+++ 2741-function-composition/function-composition.ts
@@ -1,13 +1,17 @@
 type F = (x: number) => number;
 
 function compose(functions: F[]): F {
+     if (functions.length === 0){
+        return function(x){
+            return x
+        }
+    }
     
     return function(x) {
-        let res = x;
         for(let i = functions.length - 1; i >= 0; i--){
-            res = functions[i](res)
+            x = functions[i](x)
         }
-        return res;
+        return x;
     }
 };
 
