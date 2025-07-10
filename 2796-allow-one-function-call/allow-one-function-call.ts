224a536115585c7bcfc7b433f133eef494695530
Prince Rurangwa <p.rurangwa@alustudent.com>
2025-07-10 09:30:14 +0200

    Time: 35 ms (93.77%) | Memory: 55.5 MB (21.89%) - LeetSync

---
+++ 2796-allow-one-function-call/allow-one-function-call.ts
@@ -0,0 +1,19 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    let call:number = 0;
+    return function (...args) {
+        call++;
+        if(call > 1) return undefined;
+        return fn(...args)
+    };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
