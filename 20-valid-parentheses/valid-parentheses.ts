3a62e2368da8e0f47de6b1440bd141635eccf9b0
Prince Rurangwa <p.rurangwa@alustudent.com>
2026-01-21 11:15:48 +0200

    Time: 2 ms (84.66%) | Memory: 56.3 MB (94.55%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.ts
@@ -0,0 +1,28 @@
+function isValid(s: string): boolean {
+    const stack: string[] = []
+    const matching: Record<string, string> = {
+        "}":"{",
+        ")":"(",
+        "]":"["
+    }
+    for(let i=0; i<s.length; i++){
+        let char = s[i]
+
+        if(char in matching){
+            if(stack.length === 0){
+                return false
+            }
+
+            const top = stack.pop()
+
+            if(top !== matching[char]){
+                return false
+            }
+
+        }else{
+            stack.push(char)
+        }
+    }
+
+    return stack.length === 0
+};
